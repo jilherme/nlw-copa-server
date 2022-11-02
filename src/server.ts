b78cba04092bcfcdc1f84194d67c98d417293fde
GuilhermeRos4 <guilherme.rosa@paipe.co>
2022-11-02 10:01:38 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  // baseUrl = localhost:3333
+  // {{baseUrl}}/pools/count
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+
+    return count
+  })
+
+  await fastify.listen({port: 3333, host: '0.0.0.0'});
+}
+
+bootstrap();
